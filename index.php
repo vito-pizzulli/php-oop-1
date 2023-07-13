1d07630b19f1f69a55b02f527731ca03e6a8cc1c
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-13 14:50:38 +0200

    movies class created, instance variables defined

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
-    
+    class Movies {
+        public $title;
+        public $original_title;
+        public $description;
+        public $rating;
+        public $release_date;
+        public $duration;
+        public $language;
+    }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP8 1</title>
+</head>
+<body>
+    
+</body>
+</html>
