8498dcdd80013e29c4d4c3b7c2b9029bfc0b5eb1
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-13 16:47:17 +0200

    init Movie.php, added Movie class with instance variables, constructor & getMovieInfo method

---
+++ Models/Movie.php
@@ -0,0 +1,34 @@
+<?php
+    class Movie {
+        public $title;
+        public $original_title;
+        public $plot;
+        public $genre;
+        public $rating;
+        public $release_date;
+        public $length_in_minutes;
+        public $language;
+
+        function __construct(String $title, String $original_title, String $plot, Genres $genre, String $rating, String $release_date, Int $length_in_minutes, String $language) {
+            $this->title = $title;
+            $this->original_title = $original_title;
+            $this->plot = $plot;
+            $this->genre = $genre;
+            $this->rating = $rating;
+            $this->release_date = $release_date;
+            $this->length_in_minutes = $length_in_minutes;
+            $this->language = $language;
+        }
+
+        public function getMovieInfo(){
+            echo '<ul><li> Titolo: ' . $this->title . '</li>';
+            echo '<li> Titolo originale: ' . $this->original_title . '</li>';
+            echo '<li> Trama: ' . $this->plot . '</li>';
+            echo '<li> Genere/i: ' . $this->genre->genre_1 . " " . $this->genre->genre_2 . " " . $this->genre->genre_3 . '</li>';
+            echo '<li> Voto: ' . $this->rating . '</li>';
+            echo '<li> Data di uscita: ' . $this->release_date . '</li>';
+            echo '<li> Lunghezza: ' . $this->length_in_minutes . ' minuti </li>';
+            echo '<li> Lingua: ' . $this->language . '</li></ul>';
+        }
+    }
+?>
