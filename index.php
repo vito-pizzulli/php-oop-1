200ce1163ec97a907591b10c7072ca0a08a9b14e
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-13 15:02:30 +0200

    changed class name from Movies to Movie, added genre instance variable

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
-    class Movies {
+    class Movie {
         public $title;
         public $original_title;
         public $description;
+        public $genre;
         public $rating;
         public $release_date;
         public $length_in_minutes;
         public $language;
 
-        function __construct(String $title, String $original_title, String $description, String $rating, String $release_date, Int $length_in_minutes, String $language) {
+        function __construct(String $title, String $original_title, String $description, String $genre, String $rating, String $release_date, Int $length_in_minutes, String $language) {
             $this->title = $title;
             $this->original_title = $original_title;
             $this->description = $description;
+            $this->genre = $genre;
             $this->rating = $rating;
             $this->release_date = $release_date;
             $this->length_in_minutes = $length_in_minutes;
